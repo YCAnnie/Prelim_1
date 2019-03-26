804c27969dd4f00a47b3100d6fd163092a604763
YCAnnie <47334613+YCAnnie@users.noreply.github.com>
2019-03-26 13:05:43 -0400

    Add files via upload

---
+++ p2b/p2b_14.jl
@@ -0,0 +1,27 @@
+# Sample script to run a model using GRNSimKit -
+using GRNSimKit
+
+# time step
+time_step_size = 0.016 # hr
+# Build a data dictionary from a model file -
+path_to_model_file = "/Users/anniechien/Desktop/new14.json"
+#gdd = build_default_data_dictionary(path_to_model_file)
+ddd = build_discrete_dynamic_data_dictionary_from_model_file(time_step_size, path_to_model_file)
+# Run the model to steady-state, before we do anything -
+steady_state = GRNSteadyStateSolve(ddd)
+# steady_state = dd[:initial_condition_array]
+# Run the model 100 time units *before* we add inducer -
+ddd[:initial_condition_array] = steady_state
+(T0, X0_14) = GRNDiscreteDynamicSolve((0.0,1.0,time_step_size), ddd)
+# Add inducer T = 1 mM for 100 time units -
+ddd[:initial_condition_array] = X0[end,:]
+ddd[:initial_condition_array][7] = 10.0
+tstart_1 = T0[end]
+tstop_1 = tstart_1 + 5.0
+(T1, X1_14) = GRNDiscreteDynamicSolve((tstart_1,tstop_1, time_step_size), ddd)
+# # Wash inducer out -
+# ddd[:initial_condition_array] = X1[end,:]
+# ddd[:initial_condition_array][7] = 10.0
+# tstart_2 = T1[end]
+# tstop_2 = tstart_2 + 2.0
+# (T2, X2) = GRNDiscreteDynamicSolve((tstart_2,tstop_2, time_step_size), ddd)
